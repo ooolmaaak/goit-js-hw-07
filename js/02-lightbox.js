b6e56e6be4f192a2af1e8df522805e476731fed9
ooolmaaak <131387966+ooolmaaak@users.noreply.github.com>
2024-01-29 15:42:59 +0200

    added slider (simpleLightBox)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const imagesMarkup = createGalleryImagesMurkup(galleryItems);
+
+function createGalleryImagesMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            
+          />
+        </a>
+      </li>
+    `;
+    })
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerCLick);
+
+// добавляем параметры выводящие альт-текст через х миллисекунд
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function onGalleryContainerCLick(event) {
+  gallery.on("show.simplelightbox", function () {});
+
+  event.preventDefault();
+  console.log("Open");
+}
